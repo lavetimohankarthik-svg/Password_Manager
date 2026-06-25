9ae3f2547e0515ba43fe109df3937e6431645794
lavetimohankarthik-svg <lavetimohankarthik@gmail.com>
2026-06-25 18:17:21 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <div className='bg-slate-800 text-white flex flex-col justify-center items-center  w-full'>
+            <div className="logo font-bold text-white text-2xl">
+                <span className='text-green-500'> &lt;</span>
+                <span>Pass</span><span className='text-green-500'>OP/&gt;</span>
+            </div>
+            <div className='flex justify-center items-center'> Created by Laveti Mohan Karthik </div>
+        </div>
+    )
+}
+
+export default Footer
